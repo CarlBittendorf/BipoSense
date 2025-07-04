25a7446417e42f893fdbf16f9b785c010058df86
Carl Bittendorf <carl.bittendorf@gmail.com>
2025-07-04 16:34:03 +0200

    Add time series

--- Crowded Places/scripts/figures.jl
+++ Crowded Places/scripts/figures.jl
@@ -7,3 +7,6 @@ set_aog_theme!()
 df = d"BipoSense Crowded Places"
 
 draw_spc("figures/spc", df, VARIABLES)
+
+draw_timeseries("figures/timeseries", df,
+    add_suffixes(VARIABLES, ["AR", "LNVAR", "AVG"]))
